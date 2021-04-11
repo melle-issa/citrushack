c5d207626944f37a2dc6f6cc1f599d71b7e23c4a
Melissa Hidalgo <74077340+melle-issa@users.noreply.github.com>
2021-04-10 19:00:44 -0700

    Finished 4 functions w/ minor tweaks

--- main.cpp
+++ main.cpp
@@ -6,38 +6,72 @@ void PrintMenu(){
      cout << "1 -- Used plastic bottles" << endl;
      cout << "2 -- Used aluminum cans" << endl;
      cout << "3 -- Used plastic bags" << endl;
-     cout << "4 -- Used cardboard" << endl;
+     cout << "4 -- Drove a car" << endl;
      cout << "5 -- Used reciepts" << endl;
      cout << "6 -- Took a long shower" << endl;
      cout << "7 -- Kept lights on during the day" << endl;
      cout << "8 -- End day" << endl;
 }
 
-void Bottles(){
+int Bottles(){ // produces 82.2g or 3 oz of CO2
+     int numBot;
+     int returnVal1;
+     cout << "How many plastic bottles would you like to add? ";
+     cin >> numBot;
+     cout << endl;
+     returnVal1 = numBot * 82.2;
+     return returnVal1;
+}
+
+int Cans(){ // produces 33.8g of CO2
+     int numCans;
+     int returnVal2;
+     cout << "How many aluminum cans would you like to add? ";
+     cin >> numCans;
+     cout << endl;
+     returnVal2 = 33.8 * numCans;
+     return returnVal2;
+}
+
+int Bags(){ // produces 6g of CO2 for every gram
+    string bagType;
+    int bagWeight;
+    int returnVal3;
+    int numBags;
+    cout << "Did you use a grocery bag or snack bag?" << endl;
+    cin >> bagType;
+    cout << endl;
+     if(bagType == "grocery"){
+          bagWeight = 60;
+     }
+     else{
+          bagWeight = 8;
+     }
+     cout << "How many bags did you use? ";
+     cin >> numBags;
+     returnVal3 = (bagWeight * 6 * numBags);
+     return returnVal3;
+}
+
+int Car(){ // produces 114g of CO2 per mile
+     int miles;
+     int returnVal4;
+     cout << "How far did you drive in miles? ";
+     cin >> miles;
+     cout << endl;
+     returnVal4 = miles*114;
+     return returnVal4;
+}
+
+int Reciepts(){
     
 }
 
-void Cans(){
+int Showers(){
     
 }
 
-void Bags(){
-    
-}
-
-void Cardboard(){
-    
-}
-
-void Reciepts(){
-    
-}
-
-void Showers(){
-    
-}
-
-void Light(){
+int Light(){
     
 }
 
